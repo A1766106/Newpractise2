32f22a184874a67de51f5c4727f1323aec481be0
Max Bruce <maxbruce@student-10-201-05-154.wireless.adelaide.edu.au>
2022-10-25 11:33:59 +1030

    cheer

--- sort_by_name.h
+++ sort_by_name.h
@@ -13,21 +13,6 @@ using namespace std;
 class sort_by_name{
       public:
         static void sort(animal **animals,int n);
-        // {
-        //     for(int i=0;i<n;i++)
-        //     {
-        //         for(int j=0; j<n-i-1;j++)
-        //         {
-        //             if(animals[j]->get_name() > animals[j+1]->get_name())
-        //             {
-        //                 animal *temp = animals[j];
-        //                 animals[j] = animals[j+1];
-        //                 animals[j+1] = temp;
-
-        //             }
-        //         }
-        //     }
-        // }  
 };
 
 #endif
